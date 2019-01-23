23855acb6f2e94726c51bb17eb2110597de65041
shohei0718 <Shohei@MacBook-Pro-7.local>
2019-01-23 19:23:27 +0900

    Customerクラスを定義

--- entrance_exam/exam/models.py
+++ entrance_exam/exam/models.py
@@ -1,3 +1,8 @@
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+
+class Customer(models.Model):
+  title = models.CharField('名前', max_length = 20)
+  gender = models.CharField('性別', max_length = 2)
+  age = models.IntegerField('年齢')
