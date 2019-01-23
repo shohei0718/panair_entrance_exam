eae6a771042f9d7924cf75d0b9e3221c653d3317
shohei0718 <Shohei@MacBook-Pro-7.local>
2019-01-23 19:21:14 +0900

    5つのビュー関数の設定
    ①メニュー
    ②顧客一覧
    ③レッスン受講記録
    ④月別請求一覧
    ⑤レポート

--- entrance_exam/exam/views.py
+++ entrance_exam/exam/views.py
@@ -1,3 +1,29 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from exam.models import Customer
 
 # Create your views here.
+
+def index(request):
+  context = {'word': 'Hello, World'}
+  return render(request, 'exam/index.html', context)
+
+
+def customer_index(request):
+  columns = {'word': 'Hello, World'}
+  return render(request, 'exam/customer_index.html', columns)
+
+
+def lesson_index(request):
+  columns = {'word': 'Hello, World'}
+  return render(request, 'exam/lesson_index.html', columns)
+
+
+def invoice_index(request):
+  columns = {'word': 'Hello, World'}
+  return render(request, 'exam/invoice_index.html', columns)
+
+
+def report_index(request):
+  columns = {'word': 'Hello, World'}
+  return render(request, 'exam/report_index.html', columns)
