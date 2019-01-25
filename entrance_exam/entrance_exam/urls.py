ec0f12ee7f490c6c1417f44e2c96ed641418ecd3
shohei0718 <Shohei@MacBook-Pro-7.local>
2019-01-25 18:52:44 +0900

    urls（新規登録・編集）
    customer_form
    customer_edit

--- entrance_exam/entrance_exam/urls.py
+++ entrance_exam/entrance_exam/urls.py
@@ -18,5 +18,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('exam/', include('exam.urls')),
+    path('exam/', include('exam.urls'))
 ]
--- entrance_exam/exam/urls.py
+++ entrance_exam/exam/urls.py
@@ -2,8 +2,10 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('index', views.index),
+    path('index', views.index, name = 'index'),
     path('customer_index', views.customer_index, name = 'customer_index'),
+    path('customer_form', views.customer_form, name = 'customer_form'),
+    path('customer/<int:customer_id>/edit', views.customer_edit, name = 'customer_edit'),
     path('lesson_index', views.lesson_index, name = 'lesson_index'),
     path('invoice_index', views.invoice_index, name = 'invoice_index'),
     path('report_index', views.report_index, name = 'report_index'),
