744bd132f51abb4f13c3020f0741ad10a146e02d
shohei0718 <Shohei@MacBook-Pro-7.local>
2019-01-27 11:42:04 +0900

    Merge

--- entrance_exam/exam/forms.py
+++ entrance_exam/exam/forms.py
@@ -1,6 +1,6 @@
 from django import forms
 from exam.models import Customer
-
+from exam.models import Lesson
 
 class CustomerForm(forms.ModelForm):
   class Meta:
@@ -12,3 +12,13 @@ class CustomerEdit(forms.ModelForm):
   class Meta:
     model = Customer
     fields = ('name', 'gender', 'age',)
+
+class LessonForm(forms.ModelForm):
+  class Meta:
+    model = Lesson
+    fields = ('customer', 'genre', 'lesson_day', 'lesson_time')
+
+class LessonEdit(forms.ModelForm):
+  class Meta:
+    model = Lesson
+    fields = ('customer', 'genre', 'lesson_day', 'lesson_time')
