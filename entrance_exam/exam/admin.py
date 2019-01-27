ecd9b60998d05a6f341b2db71897ee519f72f491
shohei0718 <37667650+shohei0718@users.noreply.github.com>
2019-01-27 12:00:12 +0900

    Revert "レッスン受講記録"

--- entrance_exam/exam/admin.py
+++ entrance_exam/exam/admin.py
@@ -1,6 +1,3 @@
 from django.contrib import admin
-from .models import Customer, Lesson
-# Register your models here.
 
-admin.site.register(Customer)
-admin.site.register(Lesson)
+# Register your models here.
--- entrance_exam/exam/urls.py
+++ entrance_exam/exam/urls.py
@@ -7,8 +7,6 @@ urlpatterns = [
     path('customer_form', views.customer_form, name = 'customer_form'),
     path('customer/<int:customer_id>/edit', views.customer_edit, name = 'customer_edit'),
     path('lesson_index', views.lesson_index, name = 'lesson_index'),
-    path('lesson_form', views.lesson_form, name = 'lesson_form'),
-    path('lesson/<int:lesson_id>/edit', views.lesson_edit, name = 'lesson_edit'),
     path('invoice_index', views.invoice_index, name = 'invoice_index'),
     path('report_index', views.report_index, name = 'report_index'),
 ]
