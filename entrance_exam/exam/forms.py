64ba3eda54c1f95d73a7922f42a071d7712365f8
shohei0718 <Shohei@MacBook-Pro-7.local>
2019-02-02 21:01:04 +0900

    フォーム

--- entrance_exam/exam/forms.py
+++ entrance_exam/exam/forms.py
@@ -1,6 +1,7 @@
 from django import forms
 from exam.models import Customer
 from exam.models import Lesson
+from datetime import datetime, date
 
 class CustomerForm(forms.ModelForm):
   class Meta:
@@ -8,17 +9,11 @@ class CustomerForm(forms.ModelForm):
     fields = ('name', 'gender', 'age',)
 
 
-class CustomerEdit(forms.ModelForm):
-  class Meta:
-    model = Customer
-    fields = ('name', 'gender', 'age',)
-
 class LessonForm(forms.ModelForm):
   class Meta:
     model = Lesson
     fields = ('customer', 'genre', 'lesson_day', 'lesson_time')
 
-class LessonEdit(forms.ModelForm):
-  class Meta:
-    model = Lesson
-    fields = ('customer', 'genre', 'lesson_day', 'lesson_time')
+class InvoiceSearchForm(forms.Form):
+  lesson_day = forms.CharField(label='受講日', required=False)
+
