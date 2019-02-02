c55c30a1ae81f4722fde4cf1dda44a5631014c7a
shohei0718 <Shohei@MacBook-Pro-7.local>
2019-02-02 21:01:29 +0900

    モデル

--- entrance_exam/exam/models.py
+++ entrance_exam/exam/models.py
@@ -19,6 +19,9 @@ class Customer(models.Model):
     )
   age = models.IntegerField('年齢')
 
+  def __str__(self):
+    return self.name
+
 class Lesson(models.Model):
   English = '英語'
   Finance = 'ファイナンス'
@@ -47,3 +50,29 @@ class Lesson(models.Model):
   )
   price = models.IntegerField('支払金額', null=True)
 
+  def __str__(self):
+    return self.genre
+
+"""
+from datetime import datetime, date, timedelta
+from dateutil.relativedelta import relativedelta
+
+today = datetime.today()
+
+#今月の月初日と月末日の算出
+month_start = today.replace(day=1)
+next_month_start = (today + relativedelta(months=1)).replace(day=1)
+month_end = next_month_start - timedelta(days=1)
+
+#先月の月初日と月末日の算出
+one_month_ago_start = (today - relativedelta(months=1)).replace(days=1)
+one_month_ago_end = next_month_start - timedelta(days=1)
+
+#2ヶ月前の月初日と月末日の算出
+two_month_ago_start = (today - relativedelta(months=2)).replace(days=1)
+two_month_ago_end = one_month_ago_start - timedelta(days=1)
+
+#3ヶ月前の月初日と月末日の算出
+three_month_ago_start = (today - relativedelta(months=3)).replace(days=1)
+three_month_ago_end = two_month_ago_start - timedelta(days=1)
+"""
